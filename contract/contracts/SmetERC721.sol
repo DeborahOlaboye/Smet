13ccc1b3662404ebbbed0df617c8ddb08d3ae070
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 03:17:42 -0800

    feat: add pausable and ownable functionality to SmetHero contract

--- contract/contracts/SmetERC721.sol
+++ contract/contracts/SmetERC721.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmetHero is ERC721 {
+contract SmetHero is ERC721, Pausable, Ownable {
     uint256 public nextId = 1;
 
-    constructor() ERC721("SmetHero", "SHERO") {}
+    constructor() ERC721("SmetHero", "SHERO") Ownable(msg.sender) {}
 
     function mint(address to) external returns (uint256 id) {
         id = nextId++;
