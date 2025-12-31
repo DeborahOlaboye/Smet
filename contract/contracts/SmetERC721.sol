5612c5eb69b82cb0825450cb1abcac110109114f
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 03:07:36 -0800

    feat: add input validation to SmetHero ERC721 functions

--- contract/contracts/SmetERC721.sol
+++ contract/contracts/SmetERC721.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./InputValidator.sol";
 
 contract SmetHero is ERC721 {
     uint256 public nextId = 1;
@@ -8,7 +9,31 @@ contract SmetHero is ERC721 {
     constructor() ERC721("SmetHero", "SHERO") {}
 
     function mint(address to) external returns (uint256 id) {
+        InputValidator.validateAddress(to);
         id = nextId++;
         _safeMint(to, id);
+    }
+    
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+        InputValidator.validateAddress(from);
+        InputValidator.validateAddress(to);
+        super.transferFrom(from, to, tokenId);
+    }
+    
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override {
+        InputValidator.validateAddress(from);
+        InputValidator.validateAddress(to);
+        super.safeTransferFrom(from, to, tokenId);
+    }
+    
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override {
+        InputValidator.validateAddress(from);
+        InputValidator.validateAddress(to);
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+    
+    function approve(address to, uint256 tokenId) public override {
+        InputValidator.validateAddress(to);
+        super.approve(to, tokenId);
     }
 }
