ae4006951ca4126441cdc9773afa8ae3604ccc0f
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 02:54:01 -0800

    feat: add batch operation events to SmetGold contract for gas tracking

--- contract/contracts/SmetERC20.sol
+++ contract/contracts/SmetERC20.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SmetGold is ERC20 {
+    event BatchTransferCompleted(address indexed sender, uint256 count);
+    event BatchApprovalCompleted(address indexed sender, uint256 count);
+
     constructor() ERC20("SmetGold", "SGOLD") {
         _mint(msg.sender, 10000000 ether);
     }
@@ -12,6 +15,7 @@ contract SmetGold is ERC20 {
         for (uint256 i = 0; i < recipients.length; i++) {
             transfer(recipients[i], amounts[i]);
         }
+        emit BatchTransferCompleted(msg.sender, recipients.length);
     }
 
     function batchApprove(address[] calldata spenders, uint256[] calldata amounts) external {
@@ -19,6 +23,7 @@ contract SmetGold is ERC20 {
         for (uint256 i = 0; i < spenders.length; i++) {
             approve(spenders[i], amounts[i]);
         }
+        emit BatchApprovalCompleted(msg.sender, spenders.length);
     }
 
     function batchTransferFrom(address[] calldata from, address[] calldata to, uint256[] calldata amounts) external {
