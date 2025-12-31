fe927912fdc63138afaeda59e4c2a9a170566f84
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 02:54:46 -0800

    feat: add batch operation events to SmetLoot contract for gas tracking

--- contract/contracts/SmetERC1155.sol
+++ contract/contracts/SmetERC1155.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract SmetLoot is ERC1155 {
+    event BatchMintCompleted(address indexed minter, uint256 count);
+    event BatchTransferCompleted(address indexed sender, uint256 count);
+    
     constructor() ERC1155("https://loot.example/{id}.json") {}
 
     function mint(address to, uint256 id, uint256 amount) external {
@@ -14,6 +17,7 @@ contract SmetLoot is ERC1155 {
         for (uint256 i = 0; i < recipients.length; i++) {
             _mint(recipients[i], ids[i], amounts[i], "");
         }
+        emit BatchMintCompleted(msg.sender, recipients.length);
     }
 
     function batchTransfer(address[] calldata to, uint256[] calldata ids, uint256[] calldata amounts) external {
@@ -21,6 +25,7 @@ contract SmetLoot is ERC1155 {
         for (uint256 i = 0; i < to.length; i++) {
             safeTransferFrom(msg.sender, to[i], ids[i], amounts[i], "");
         }
+        emit BatchTransferCompleted(msg.sender, to.length);
     }
 
     function batchApproval(address[] calldata operators, bool[] calldata approved) external {
