de86e563f25d8b95765313ca6673e334ff7825b5
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 03:16:53 -0800

    feat: add pausable and ownable functionality to SmetGold contract

--- contract/contracts/SmetERC20.sol
+++ contract/contracts/SmetERC20.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmetGold is ERC20 {
-    constructor() ERC20("SmetGold", "SGOLD") {
+contract SmetGold is ERC20, Pausable, Ownable {
+    constructor() ERC20("SmetGold", "SGOLD") Ownable(msg.sender) {
         _mint(msg.sender, 10000000 ether);
     }
-}
