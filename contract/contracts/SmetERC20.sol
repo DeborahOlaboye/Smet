fe34268bac95f8804392b114bf0cc5e21550e88e
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 05:50:29 -0800

    feat: add emergency recovery features to SmetGold contract

--- contract/contracts/SmetERC20.sol
+++ contract/contracts/SmetERC20.sol
@@ -1,9 +1,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmetGold is ERC20 {
-    constructor() ERC20("SmetGold", "SGOLD") {
+contract SmetGold is ERC20, Pausable, Ownable {
+    address public emergencyRecovery;
+    
+    event EmergencyRecoverySet(address indexed recovery);
+    
+    constructor() ERC20("SmetGold", "SGOLD") Ownable(msg.sender) {
         _mint(msg.sender, 10000000 ether);
+    }
+    
+    function pause() external onlyOwner {
+        _pause();
+    }
+    
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+    
+    function setEmergencyRecovery(address _emergencyRecovery) external onlyOwner {
+        emergencyRecovery = _emergencyRecovery;
+        emit EmergencyRecoverySet(_emergencyRecovery);
+    }
+    
+    function transfer(address to, uint256 amount) public override whenNotPaused returns (bool) {
+        return super.transfer(to, amount);
+    }
+    
+    function transferFrom(address from, address to, uint256 amount) public override whenNotPaused returns (bool) {
+        return super.transferFrom(from, to, amount);
     }
 }
