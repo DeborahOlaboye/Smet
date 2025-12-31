d1efdf844a90944a1c20490e066cbe7c7fdae9ea
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 06:34:50 -0800

    feat: add circuit breaker to SmetGold ERC20 token transfers

--- contract/contracts/SmetERC20.sol
+++ contract/contracts/SmetERC20.sol
@@ -1,9 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./CircuitBreaker.sol";
 
-contract SmetGold is ERC20 {
+contract SmetGold is ERC20, CircuitBreaker {
     constructor() ERC20("SmetGold", "SGOLD") {
         _mint(msg.sender, 10000000 ether);
+    }
+
+    function transfer(address to, uint256 value) public override circuitBreakerCheck(this.transfer.selector) returns (bool) {
+        return super.transfer(to, value);
+    }
+
+    function transferFrom(address from, address to, uint256 value) public override circuitBreakerCheck(this.transferFrom.selector) returns (bool) {
+        return super.transferFrom(from, to, value);
+    }
+
+    function approve(address spender, uint256 value) public override circuitBreakerCheck(this.approve.selector) returns (bool) {
+        return super.approve(spender, value);
     }
 }
