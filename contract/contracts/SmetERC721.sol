7425ef9050b9d3a3545c1a5cd7fa5cdb5eb447ce
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 02:58:37 -0800

    feat: add formal verification setup to SmetHero contract

--- contract/contracts/SmetERC721.sol
+++ contract/contracts/SmetERC721.sol
@@ -4,8 +4,13 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SmetHero is ERC721 {
     uint256 public nextId = 1;
+    uint256 private totalMinted = 0;
 
-    constructor() ERC721("SmetHero", "SHERO") {}
+    constructor() ERC721("SmetHero", "SHERO") {
+        // Formal verification: Initial state
+        assert(nextId == 1);
+        assert(totalMinted == 0);
+    }
 
     function mint(address to) external returns (uint256 id) {
         id = nextId++;
