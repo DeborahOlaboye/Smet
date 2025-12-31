a19e8bfa0427b79ec0c33acb621255e7ec110da0
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 05:58:08 -0800

    feat: add timelock protection to SmetGold minting function

--- contract/contracts/SmetERC20.sol
+++ contract/contracts/SmetERC20.sol
@@ -1,9 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmetGold is ERC20 {
-    constructor() ERC20("SmetGold", "SGOLD") {
+contract SmetGold is ERC20, Ownable {
+    address public timelock;
+    
+    modifier onlyTimelock() {
+        require(msg.sender == timelock, "Only timelock");
+        _;
+    }
+    
+    event TimelockSet(address indexed timelock);
+    event TokensMinted(address indexed to, uint256 amount);
+    
+    constructor() ERC20("SmetGold", "SGOLD") Ownable(msg.sender) {
         _mint(msg.sender, 10000000 ether);
+    }
+    
+    function setTimelock(address _timelock) external onlyOwner {
+        timelock = _timelock;
+        emit TimelockSet(_timelock);
+    }
+    
+    function mint(address to, uint256 amount) external onlyTimelock {
+        _mint(to, amount);
+        emit TokensMinted(to, amount);
     }
 }
