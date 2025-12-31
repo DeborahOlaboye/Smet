2e7bd673f313e259c1d7d44c955d9b1a16c72e9d
victor-olamide <olaboyevictor20@gmail.com>
2025-12-31 03:02:17 -0800

    feat: add transfer verification to SmetLoot contract

--- contract/contracts/SmetERC1155.sol
+++ contract/contracts/SmetERC1155.sol
@@ -18,4 +18,19 @@ contract SmetLoot is ERC1155 {
         assert(balanceOf(to, id) == previousBalance + amount);
         assert(totalSupplyById[id] == previousTotalSupply + amount);
     }
+    
+    function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public override {
+        uint256 fromBalanceBefore = balanceOf(from, id);
+        uint256 toBalanceBefore = balanceOf(to, id);
+        uint256 totalSupplyBefore = totalSupplyById[id];
+        
+        super.safeTransferFrom(from, to, id, amount, data);
+        
+        // Formal verification: Transfer correctness
+        if (from != to) {
+            assert(balanceOf(from, id) == fromBalanceBefore - amount);
+            assert(balanceOf(to, id) == toBalanceBefore + amount);
+        }
+        assert(totalSupplyById[id] == totalSupplyBefore);
+    }
 }
