acf0d7c88c741f1db54ff0d34ca4abda033e6605
victor-olamide <olaboyevictor20@gmail.com>
2026-01-03 13:47:40 -0800

    feat: Update SmetLoot contract to support IPFS metadata URIs

--- contract/contracts/SmetERC1155.sol
+++ contract/contracts/SmetERC1155.sol
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmetLoot is ERC1155 {
-    constructor() ERC1155("https://loot.example/{id}.json") {}
+contract SmetLoot is ERC1155, Ownable {
+    string private _baseTokenURI;
+
+    constructor(string memory baseURI) ERC1155("") Ownable(msg.sender) {
+        _baseTokenURI = baseURI;
+    }
 
     function mint(address to, uint256 id, uint256 amount) external {
         _mint(to, id, amount, "");
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(_baseTokenURI, tokenId, ".json"));
     }
 }
