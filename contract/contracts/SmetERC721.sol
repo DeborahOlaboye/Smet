84a6102c8d2a5125e16c65cff79b5f59bf752706
victor-olamide <olaboyevictor20@gmail.com>
2026-01-03 13:47:27 -0800

    feat: Update SmetHero contract to support IPFS metadata URIs

--- contract/contracts/SmetERC721.sol
+++ contract/contracts/SmetERC721.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SmetHero is ERC721 {
+contract SmetHero is ERC721, Ownable {
     uint256 public nextId = 1;
+    string private _baseTokenURI;
 
-    constructor() ERC721("SmetHero", "SHERO") {}
+    constructor(string memory baseURI) ERC721("SmetHero", "SHERO") Ownable(msg.sender) {
+        _baseTokenURI = baseURI;
+    }
 
     function mint(address to) external returns (uint256 id) {
         id = nextId++;
         _safeMint(to, id);
+    }
+
+    function setBaseURI(string memory baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(ownerOf(tokenId) != address(0), "Token does not exist");
+        return string(abi.encodePacked(_baseURI(), tokenId, ".json"));
     }
 }
