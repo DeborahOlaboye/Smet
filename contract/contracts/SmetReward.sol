452cbd81ddf1350fbae98307a3a81b45db745b65
victor-olamide <olaboyevictor20@gmail.com>
2026-01-05 14:32:15 -0800

    feat: add dynamic reward pool management with admin controls

--- contract/contracts/SmetReward.sol
+++ contract/contracts/SmetReward.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
@@ -18,7 +19,8 @@ struct Reward {
 contract SmetReward is 
     VRFConsumerBaseV2Plus, 
     IERC721Receiver, 
-    IERC1155Receiver 
+    IERC1155Receiver,
+    Ownable 
 {
     address public immutable VRF_COORD;
     bytes32 public immutable keyHash;
@@ -36,6 +38,11 @@ contract SmetReward is
 
     event Opened(address indexed opener, uint256 indexed reqId);
     event RewardOut(address indexed opener, Reward reward);
+    event RewardAdded(uint256 indexed index, Reward reward, uint32 weight);
+    event RewardRemoved(uint256 indexed index);
+    event RewardUpdated(uint256 indexed index, Reward reward, uint32 weight);
+    event WeightsUpdated();
+    event FeeUpdated(uint256 newFee);
 
     constructor(
         address _coordinator,
@@ -44,7 +51,7 @@ contract SmetReward is
         uint256 _fee,
         uint32[] memory _weights,
         Reward[] memory _prizes
-    ) VRFConsumerBaseV2Plus(_coordinator) {
+    ) VRFConsumerBaseV2Plus(_coordinator) Ownable(msg.sender) {
         require(_weights.length == _prizes.length && _weights.length > 0, "len mismatch");
         VRF_COORD = _coordinator;
         subId = _subId;
@@ -121,6 +128,97 @@ contract SmetReward is
     function refill(IERC20 token, uint256 amount) external {
         require(amount > 0, "!amount");
         token.transferFrom(msg.sender, address(this), amount);
+    }
+
+    // ===== DYNAMIC REWARD POOL MANAGEMENT =====
+
+    function addReward(Reward memory reward, uint32 weight) external onlyOwner {
+        require(weight > 0, "!weight");
+        require(reward.assetType >= 1 && reward.assetType <= 3, "invalid assetType");
+        
+        prizePool.push(reward);
+        
+        if (cdf.length == 0) {
+            cdf.push(weight);
+        } else {
+            cdf.push(cdf[cdf.length - 1] + weight);
+        }
+        
+        emit RewardAdded(prizePool.length - 1, reward, weight);
+    }
+
+    function removeReward(uint256 index) external onlyOwner {
+        require(index < prizePool.length, "invalid index");
+        
+        // Remove from prize pool
+        for (uint i = index; i < prizePool.length - 1; i++) {
+            prizePool[i] = prizePool[i + 1];
+        }
+        prizePool.pop();
+        
+        emit RewardRemoved(index);
+        
+        // Rebuild CDF if rewards remain
+        if (prizePool.length > 0) {
+            _rebuildCDF();
+        } else {
+            delete cdf;
+        }
+    }
+
+    function updateReward(uint256 index, Reward memory reward, uint32 weight) external onlyOwner {
+        require(index < prizePool.length, "invalid index");
+        require(weight > 0, "!weight");
+        require(reward.assetType >= 1 && reward.assetType <= 3, "invalid assetType");
+        
+        prizePool[index] = reward;
+        emit RewardUpdated(index, reward, weight);
+        
+        _rebuildCDF();
+    }
+
+    function updateWeights(uint32[] memory weights) external onlyOwner {
+        require(weights.length == prizePool.length, "len mismatch");
+        require(weights.length > 0, "empty weights");
+        
+        delete cdf;
+        uint32 acc = 0;
+        for (uint i = 0; i < weights.length; i++) {
+            require(weights[i] > 0, "!weight");
+            acc += weights[i];
+            cdf.push(acc);
+        }
+        
+        emit WeightsUpdated();
+    }
+
+    function updateFee(uint256 newFee) external onlyOwner {
+        fee = newFee;
+        emit FeeUpdated(newFee);
+    }
+
+    function _rebuildCDF() private {
+        // This function requires external call to updateWeights with current weights
+        // or manual weight management by admin
+    }
+
+    // ===== VIEW FUNCTIONS =====
+
+    function getRewardCount() external view returns (uint256) {
+        return prizePool.length;
+    }
+
+    function getReward(uint256 index) external view returns (Reward memory) {
+        require(index < prizePool.length, "invalid index");
+        return prizePool[index];
+    }
+
+    function getAllRewards() external view returns (Reward[] memory) {
+        return prizePool;
+    }
+
+    function getWeights() external view returns (uint32[] memory) {
+        return cdf;
     }
 
     receive() external payable {}
